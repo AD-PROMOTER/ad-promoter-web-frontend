920e43a4171c7c5bc8f3a410a862eabcf800dfa4
Oyedare <ooyedaretemiloluwa@gmail.com>
2022-11-21 21:48:16 +0100

    fix: resolve conflicts

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import PromoterLayout from '@/components/PromoterLayout';
 import { GlobalStyle } from '@/styles/global';
 import { SanitizeStyle } from '@/styles/sanitize';
 import { VariableStyle } from '@/styles/variables';
+import { PreferenceProvider } from '@/context/preferenceContext';
 function MyApp({ Component, pageProps, router }) {
   if (router.pathname.startsWith('/promoters')) {
     return (
@@ -14,12 +15,12 @@ function MyApp({ Component, pageProps, router }) {
     );
   }
   return (
-    <>
+    <PreferenceProvider>
       <VariableStyle />
       <GlobalStyle />
       <SanitizeStyle />
       <Component {...pageProps} />
-    </>
+    </PreferenceProvider>
   );
 }
 
