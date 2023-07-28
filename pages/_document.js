b7bf33f1547d606cc873d93c550f4076bbad94f4
Oyedare <ooyedaretemiloluwa@gmail.com>
2023-07-28 21:30:35 +0100

    open graph fix

--- pages/_document.js
+++ pages/_document.js
@@ -64,10 +64,7 @@ export default class MyDocument extends Document {
             property="og:title"
             content="Explore The World Of Seamless Online Promotion"
           />
-          <meta
-            property="og:image"
-            content="https://adpromoter.s3.amazonaws.com/93dc0ed5-d887-49ed-b011-44f21b690984-1200%20x%20630%20Open%20graph%20image%20.png"
-          />
+          <meta property="og:image" content="https://imgur.com/a/xWyfhDK" />
           <meta
             property="og:description"
             content="AD-Promoter is a digital serviced based app that offers a result-oriented strategy for global brand growth. It creates unlimited access to transform brands into bigger ones by allowing users to reach multiple target audiences in various locations with a few clicks and a custom budget."
@@ -78,7 +75,7 @@ export default class MyDocument extends Document {
           {/* Fallback image in case the og:image is not available */}
           <meta
             property="og:image:secure_url"
-            content="https://adpromoter.s3.amazonaws.com/93dc0ed5-d887-49ed-b011-44f21b690984-1200%20x%20630%20Open%20graph%20image%20.png"
+            content="https://imgur.com/a/xWyfhDK"
           />
 
           {/* Twitter card meta tags (optional, for Twitter sharing) */}
@@ -92,10 +89,7 @@ export default class MyDocument extends Document {
             name="twitter:description"
             content="AD-Promoter is a digital serviced based app that offers a result-oriented strategy for global brand growth. It creates unlimited access to transform brands into bigger ones by allowing users to reach multiple target audiences in various locations with a few clicks and a custom budget."
           />
-          <meta
-            name="twitter:image"
-            content="https://adpromoter.s3.amazonaws.com/93dc0ed5-d887-49ed-b011-44f21b690984-1200%20x%20630%20Open%20graph%20image%20.png"
-          />
+          <meta name="twitter:image" content="https://imgur.com/a/xWyfhDK" />
         </Head>
         <body>
           <Main />
